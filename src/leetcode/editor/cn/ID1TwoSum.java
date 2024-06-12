cba3a22a5caeeaad26749ea4db7a8d87c625a50c
luanhongmin <luanhongmin@58.com>
2024-06-12 14:54:25 +0800

    init

---
+++ src/leetcode/editor/cn/ID1TwoSum.java
@@ -0,0 +1,23 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+public class ID1TwoSum{
+	public static void main(String[] args) {
+		Solution solution = new ID1TwoSum().new Solution();
+		StringBuilder sb = new StringBuilder();
+		
+		//执行测试
+		
+		System.out.println(sb);		
+	}
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+		System.out.println(11);
+		System.out.println(22);
+		return new int[]{1,2};
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
